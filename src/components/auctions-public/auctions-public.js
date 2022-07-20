2d63f6b9649320e9f291f19aef17068003043e9f
hassendabb <hassen.dabbabi@target-energysolutions.com>
2022-07-20 10:42:11 +0100

    add upcominf auction data for home page

--- src/components/auctions-public/auctions-public.js
+++ src/components/auctions-public/auctions-public.js
@@ -5,24 +5,23 @@
 
 import HomeSlider from 'components/home-slider'
 
-import { dummyData } from './helper.js'
 import { useQuery } from 'react-query'
-import { featuredAuctions } from 'libs/api/auctions-api'
+import { featuredAuctions, listAuction } from 'libs/api/auctions-api'
 import UpcomingAuctions from 'components/upcoming-auctions/upcoming-auctions.js'
 
 const AuctionsPublic = () => {
   //
 
   const { data: featAuctions } = useQuery('featuredAuctions', featuredAuctions)
-  // const { data: upcomingAuctionsData } = useQuery(
-  //   ['UpcomingAuctions', 'Upcoming'],
-  //   listAuction,
-  // )
+  const { data: upcomingAuctionsData } = useQuery(
+    ['UpcomingAuctions', 'Upcoming'],
+    listAuction,
+  )
 
   return (
     <>
       <HomeSlider auctions={featAuctions?.results} />
-      <UpcomingAuctions cards={dummyData} />
+      <UpcomingAuctions cards={upcomingAuctionsData?.results} />
     </>
   )
 }
