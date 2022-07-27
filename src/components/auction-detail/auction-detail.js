e318be1878576c338a43b7f2ac74f773e3ba46cc
abdesslam <abdesslam@pop-os.localdomain>
2022-07-27 14:31:53 +0100

    new bid subscriptions and geting bidders list in my auctions

--- src/components/auction-detail/auction-detail.js
+++ src/components/auction-detail/auction-detail.js
@@ -40,7 +40,10 @@ const AuctionDetail = ({ auctionId, isAdmin, status }) => {
   const downloadToken = store?.getState()?.app?.dlToken
 
   const [docAction, setDocAction] = useState(false)
-  const { data: auctionData } = useQuery(['auctionData', auctionId], getAuction)
+  const { data: auctionData, refetch: refetchAuction } = useQuery(
+    ['auctionData', auctionId],
+    getAuction,
+  )
   const { data: auctionPropertyData } = useQuery(
     ['auctionProperty', auctionId],
     auctionProperty,
@@ -64,7 +67,7 @@ const AuctionDetail = ({ auctionId, isAdmin, status }) => {
   //   variables: { auctionID: auctionId },
   // })
   useEffect(() => {
-    // refetchBid()
+    refetchAuction()
   }, [subNewBid])
 
   const isActive = useMemo(
@@ -296,7 +299,7 @@ const AuctionDetail = ({ auctionId, isAdmin, status }) => {
               className="auction-details-btn"
               onClick={() =>
                 // isParticipant ? setBidDialog(true) : setTermsDialog(true)
-                setTermsDialog(true)
+                setBidDialog(true)
               }
             >
               {t('bid_now')}
