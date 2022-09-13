8c38a38dfa5a47ef77b8387d26cc5c2fc1198a91
zouBall <zouhour.balloum@target-energysolutions.com>
2022-09-13 16:14:40 +0100

    fix: active tab in navbar

--- src/components/top-bar/top-bar.js
+++ src/components/top-bar/top-bar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { cls } from 'reactutils'
 import { get } from 'lodash-es'
 
@@ -35,7 +35,7 @@ const TopBar = ({
   onClickRegisterUrl,
   modulesList,
   className,
-  currentModule,
+
   onClickModule,
   logged,
   clear,
@@ -46,6 +46,7 @@ const TopBar = ({
   const langs = useSupportedLangs()
   const changeLang = useChangeLanguage()
   const [openMenu, setOpenMenu] = useState(false)
+  const [currentModule, setCurrentModule] = useState('')
   // const [auctionsMenu, setAuctionsMenu] = useState(false)
   const [notifPanelVisible, setNotifPanelVisible] = useState(false)
 
@@ -54,6 +55,22 @@ const TopBar = ({
     ? user?.profile?.fullName.match(/\b(\w)/g)?.join('')
     : null
   const modules = location.pathname.split('/').filter((v) => v !== '')
+  useEffect(() => {
+    if (
+      modules?.includes('live-auctions') ||
+      modules?.includes('upcoming-auctions')
+    ) {
+      setCurrentModule('auctions')
+    } else {
+      setCurrentModule(
+        modulesList?.find(
+          (el) =>
+            el?.linkToNewTab === modules[1] ||
+            el?.subMenu?.find((s) => s?.link === modules[1]),
+        )?.key,
+      )
+    }
+  }, [])
   const getActiveLabel = ({ activeLabel }) => {
     if (activeLabel === 'اللغة العربية') {
       return 'عربي'
@@ -74,6 +91,7 @@ const TopBar = ({
                 primaryText={label}
                 onClick={() => {
                   onClickModule && onClickModule(key)
+
                   link && link()
                   navigate(linkToNewTab)
                 }}
@@ -124,25 +142,27 @@ const TopBar = ({
             id="smart-menu-button-1"
             className={cls(
               'modules-item',
-              // currentModule === key ? 'active' : '',
+              currentModule === 'auctions' ? 'active' : '',
             )}
             flat
             menuItems={[
               <ListItem
                 key={1}
                 primaryText="Live Auctions"
-                onClick={() =>
+                onClick={() => {
                   navigate(`/${logged ? 'auctions' : 'public'}/live-auctions`)
-                }
+                  setCurrentModule('auctions')
+                }}
               />,
               <ListItem
                 key={2}
                 primaryText="Upcoming Auctions"
-                onClick={() =>
+                onClick={() => {
                   navigate(
                     `/${logged ? 'auctions' : 'public'}/upcoming-auctions`,
                   )
-                }
+                  setCurrentModule('auctions')
+                }}
               />,
             ]}
             simplifiedMenu={false}
@@ -201,6 +221,7 @@ const TopBar = ({
                           onClick={() => {
                             onClickModule && onClickModule(key)
                             link && link()
+                            setCurrentModule(key)
                           }}
                         >
                           {label}
@@ -210,20 +231,21 @@ const TopBar = ({
                       <MenuButton
                         className={cls(
                           'modules-item',
-                          // currentModule === key ? 'active' : '',
+                          currentModule === key ? 'active' : '',
                         )}
                         flat
                         menuItems={subMenu?.map((el) => (
                           <ListItem
                             key={1}
                             primaryText={el?.label}
-                            onClick={() =>
+                            onClick={() => {
                               navigate(
                                 logged
                                   ? `/auctions/${el?.link}`
                                   : `/public/${el?.link}`,
                               )
-                            }
+                              setCurrentModule(key)
+                            }}
                           />
                         ))}
                         simplifiedMenu={false}
