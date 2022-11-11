346f8cff866b1731578a4fe09b02a746906154c8
Mohamed Mahfoudhi <mohamed.mahfoudhi@target-energysolutions.com>
2022-11-11 16:09:20 +0100

    add popup actions

--- src/components/auction-asset/auction-asset.js
+++ src/components/auction-asset/auction-asset.js
@@ -1,5 +1,4 @@
 import Auctions from 'components/auctions'
-import Calendar from 'components/calendar'
 import { useTranslation } from 'libs/langs'
 
 import './styles.scss'
@@ -9,7 +8,6 @@ const AuctionAsset = () => {
 
   return (
     <div className="auction-asset">
-      <Calendar />
       <div className="auction-asset-title">{t('auction_asset')}</div>
       <div className="auction-asset-subTitle">{t('fill_infos')}</div>
       <Auctions />
