c24fa9dbe8448bc08b881b8c40e2b3bd8f22772b
ZaraiBilel <zaraibilel1@gmail.com>
2022-11-11 16:58:43 +0100

    fix days

--- src/components/contact-info-dialog-days/contact-info-dialog-days.js
+++ src/components/contact-info-dialog-days/contact-info-dialog-days.js
@@ -1,14 +1,47 @@
+import { useState } from 'react'
 import { Button, Checkbox, DialogContainer } from 'react-md'
-
 import './style.scss'
 
+// const [days, setDays] = useState(['Sunday', 'Monday', 'Tuesday'])
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
+
 const ContactInfoDialogdays = ({ visible, onHide }) => {
+  const [checked, setChecked] = useState([])
+  const handleChange = (day) => {
+    // test if day in checked ? // remove : //
+
+    //  setChecked(prev => prev.includes(day) ? [...prev.splice(day, 1), prev] : [...prev, day])
+    setChecked((prev) =>
+      prev?.includes(day) ? prev.filter((el) => el !== day) : [...prev, day],
+    )
+  }
+
+  // const handleChange = (day) => {
+  //   // test if day in checked ? // remove : //
+
+  //   setChecked(prev => {
+  //     if (prev.includes(day)) { prev.splice(day, 1) } else [...prev,day]
+
+  //     // prev.includes(day) ? prev.splice(day, 1) : [...prev, day]
+  //   }
+
+  //   )
+  // }
+
   return (
     <DialogContainer
       visible={visible}
       onHide={onHide}
       focusOnMount={false}
-      className="contact-info-dialog"
+      className="contact-info-dialog md-cell md-cell--3"
     >
       <div>
         <div>
@@ -17,17 +50,21 @@ const ContactInfoDialogdays = ({ visible, onHide }) => {
           </div>
           <div>
             <div className="days">
-              <Checkbox label="Sunday" checked={true} />
-              <Checkbox label="Monday" />
-              <Checkbox label="Tuesday" />
-              <Checkbox label="Wednesday" />
-              <Checkbox label="Thursday" />
-              <Checkbox label="Friday" />
-              <Checkbox label="Saturday" />
+              <div className="owner-card md-cell md-cell--3">
+                {days.map((day, i) => (
+                  <Checkbox
+                    id={`${day}-${i}`}
+                    key={i}
+                    checked={checked?.includes(day)}
+                    label={day}
+                    onChange={() => handleChange(day)}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        <div>
+        <div className="owner-card md-cell ">
           <Button
             flat
             primary
