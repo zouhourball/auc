944afa0b7148ec7335abdd54df0ff83ac14fbeac
Mohamed Mahfoudhi <mohamed.mahfoudhi@target-energysolutions.com>
2022-12-22 11:38:35 +0100

    fix flag

--- src/components/profile-page/components/profile-body/security.js
+++ src/components/profile-page/components/profile-body/security.js
@@ -579,10 +579,9 @@ const ChangeNumberDialog = ({ visible, onHide, userInfo }) => {
                     width={25}
                     src={
                       countriesCodes?.find(
-                        (el) =>
-                          el?.value === phoneNumber?.currentCode ||
-                          el?.value === '+968',
-                      )?.flag
+                        (el) => el?.value === phoneNumber?.currentCode,
+                      )?.flag ||
+                      countriesCodes?.find((el) => el?.value === '+968')?.flag
                     }
                   />
                 }
